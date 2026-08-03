7a39764ed8fb62637065782c1d2ba97c1c4da897
Zohreh Zakeran <zhrzkrneng@gmail.com>
2026-08-03 17:44:27 +0000

    feat: add vector store interface

--- src/retrieval/__init__.py
+++ src/retrieval/__init__.py
@@ -1,15 +1,7 @@
-"""Retrieval interfaces and implementations."""
-
-from src.retrieval.base_retriever import BaseRetriever
-from src.retrieval.base_vector_store import BaseVectorStore
-from src.retrieval.dense_retriever import DenseRetriever
-from src.retrieval.faiss_vector_store import FAISSVectorStore
 from src.retrieval.result import RetrievalResult
+from src.retrieval.base_vector_store import BaseVectorStore
 
 __all__ = [
-    "BaseRetriever",
-    "BaseVectorStore",
-    "DenseRetriever",
-    "FAISSVectorStore",
     "RetrievalResult",
+    "BaseVectorStore",
 ]
